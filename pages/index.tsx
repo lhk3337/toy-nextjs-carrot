8cb80f98d9548452bf22e6375c96a695358f1663
lhk3337 <lhk3337@naver.com>
2022-09-05 18:03:07 +0900

    [FEAT] tailwind item 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,27 @@
 import type { NextPage } from "next";
 const Home: NextPage = () => {
-  return <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Submits</button>;
+  return (
+    <div className="flex flex-col space-y-10 bg-slate-300 px-10 py-20">
+      <div className="rounded-xl bg-white p-4 shadow-2xl">
+        <span className="text-xl font-bold">Select Item</span>
+        <div className="my-2 flex justify-between">
+          <span className="text-gray-400">Grey Chair</span>
+          <span className="font-semibold">$18</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-400">Tooly Table</span>
+          <span className="font-semibold">$80</span>
+        </div>
+        <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
+          <span>Total</span>
+          <span className="font-semibold">$98</span>
+        </div>
+        <div className="mx-auto mt-5 w-1/2 rounded-3xl bg-blue-400 py-3 text-center text-white">Checkout</div>
+      </div>
+      <div className="rounded-xl bg-white p-10 shadow-2xl"></div>
+      <div className="rounded-xl bg-white p-10 shadow-2xl"></div>
+      <div className="rounded-xl bg-white p-10 shadow-2xl"></div>
+    </div>
+  );
 };
 export default Home;
