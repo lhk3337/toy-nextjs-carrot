a967a04fafd60ace66791cfc6e6b46a59cddea35
lhk3337 <lhk3337@naver.com>
2022-09-05 16:03:21 +0900

    [FEAT] tailwind css init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from "next";
-
 const Home: NextPage = () => {
-  return null;
+  return <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Submits</button>;
 };
-
 export default Home;
