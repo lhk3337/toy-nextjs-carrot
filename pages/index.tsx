b18bc8ff3ba2e955e0963a5d1d113d2e14d75eb9
lhk3337 <lhk3337@naver.com>
2022-09-05 20:11:38 +0900

    [FEAT] tailwind item 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col space-y-10 bg-slate-300 px-10 py-20">
-      <div className="rounded-xl bg-white p-4 shadow-2xl">
+    <div className="flex flex-col space-y-10 bg-slate-300 p-20">
+      <div className="rounded-3xl bg-white p-8 shadow-2xl">
         <span className="text-xl font-bold">Select Item</span>
         <div className="my-2 flex justify-between">
           <span className="text-gray-400">Grey Chair</span>
@@ -18,7 +18,28 @@ const Home: NextPage = () => {
         </div>
         <div className="mx-auto mt-5 w-1/2 rounded-3xl bg-blue-400 py-3 text-center text-white">Checkout</div>
       </div>
-      <div className="rounded-xl bg-white p-10 shadow-2xl"></div>
+      <div className="overflow-hidden rounded-3xl bg-white shadow-2xl">
+        <div className="bg-blue-400 p-8 pb-14">
+          <span className="text-xl font-medium text-white">Profile</span>
+        </div>
+        <div className="relative -top-8 rounded-3xl bg-white p-6">
+          <div className="relative  -top-16 flex items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-400">Orders</span>
+              <span className=" font-medium">$340</span>
+            </div>
+            <div className="h-24 w-24 rounded-full bg-[#E1EFFF]" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-400">Spent</span>
+              <span className=" font-medium">$340</span>
+            </div>
+          </div>
+          <div className="relative -mb-5 -mt-10 flex flex-col items-center">
+            <span className="text-lg font-bold">Tony Molloy</span>
+            <span className="text-sm text-gray-400">USA</span>
+          </div>
+        </div>
+      </div>
       <div className="rounded-xl bg-white p-10 shadow-2xl"></div>
       <div className="rounded-xl bg-white p-10 shadow-2xl"></div>
     </div>
