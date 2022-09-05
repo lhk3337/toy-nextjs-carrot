ac9b87bbe73c16eea1c5c5d0936f6c85d2904c50
lhk3337 <lhk3337@naver.com>
2022-09-05 23:42:01 +0900

    [FEAT] tailwind item 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col space-y-10 bg-slate-300 p-20">
+    <div className="flex min-h-screen flex-col space-y-10 bg-slate-300 p-20">
       <div className="rounded-3xl bg-white p-8 shadow-2xl">
         <span className="text-xl font-bold">Select Item</span>
         <div className="my-2 flex justify-between">
@@ -40,8 +41,40 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-      <div className="rounded-xl bg-white p-10 shadow-2xl"></div>
-      <div className="rounded-xl bg-white p-10 shadow-2xl"></div>
+      <div className="rounded-3xl bg-white p-8 px-6 shadow-2xl">
+        <div className="mb-5 flex justify-between">
+          <span>←</span>
+          <div className="flex items-center space-x-3">
+            <span>⭐️ 4.9</span>
+            <div className="flex h-8 w-8 items-center justify-center rounded-lg shadow-lg">
+              <span>❤️</span>
+            </div>
+          </div>
+        </div>
+        <div className="my-5 flex items-center justify-center shadow-2xl shadow-slate-400">
+          <Image src="https://i.ibb.co/5963kz8/chair.jpg" width={200} height={300} alt="chair" />
+        </div>
+        <div className="flex flex-col">
+          <span className="text-xl font-semibold">Swoon Lounge</span>
+          <span className="text-sm text-gray-500">Chair</span>
+          <div className="mt-3 mb-5 flex items-center justify-between">
+            <div className="space-x-2">
+              <button className="h-5 w-5 rounded-full bg-yellow-400   ring-yellow-400 ring-offset-2 transition focus:ring-2" />
+              <button className="h-5 w-5 rounded-full bg-indigo-400 ring-indigo-400 ring-offset-2 transition focus:ring-2" />
+              <button className="h-5 w-5 rounded-full bg-teal-400 ring-teal-400 ring-offset-2 transition focus:ring-2" />
+            </div>
+            <div className="flex items-center space-x-5">
+              <button className="aspect-square w-8 rounded-lg bg-blue-400 p-1.5 text-white">-</button>
+              <span>1</span>
+              <button className="aspect-square w-8 rounded-lg bg-blue-400 p-1.5 text-white">+</button>
+            </div>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-2xl font-medium">$450</span>
+            <button className="rounded-lg bg-blue-400 p-2 px-8 text-center text-sm text-white">Add to cart</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
