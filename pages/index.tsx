f144ede4f26915f22ac385a5f8cdd53384239fbb
lhk3337 <lhk3337@naver.com>
2022-09-07 21:17:01 +0900

    [FIX] index init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,5 @@
 import type { NextPage } from "next";
 const Home: NextPage = () => {
-  return (
-    <>
-      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">Submits</button>
-      <li>g</li>
-    </>
-  );
+  return null;
 };
 export default Home;
