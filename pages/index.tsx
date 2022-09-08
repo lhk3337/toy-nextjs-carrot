4c306fdd952ece196e23eed90d621557141f907e
lhk3337 <lhk3337@naver.com>
2022-09-08 15:14:00 +0900

    [FEAT] first-letter modifier

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,7 @@ import Image from "next/image";
 const Home: NextPage = () => {
   return (
     <div className="flex flex-col space-y-2 p-10">
-      <input
-        type="file"
-        className="file:cursor-pointer file:rounded-xl file:border-0 file:bg-purple-400 file:px-5 file:py-1  file:text-white  file:hover:border file:hover:border-purple-400 file:hover:bg-white file:hover:text-purple-500"
-      />
+      <p className="first-letter:text-7xl first-letter:hover:text-purple-500">Lorem ipsum dolor sit amet.</p>
     </div>
   );
 };
