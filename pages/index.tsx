b5ba9d9e758078c55fdcdabf06bd49ac7b281f76
lhk3337 <lhk3337@naver.com>
2022-09-08 14:57:49 +0900

    [FEAT] file modifier

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,11 @@ import type { NextPage } from "next";
 import Image from "next/image";
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col space-y-2 p-5">
-      <details className="select-none open:bg-indigo-300 open:text-white">
-        <summary className="cursor-pointer">What is my fav. food.</summary>
-        <span>kimchi</span>
-      </details>
+    <div className="flex flex-col space-y-2 p-10">
+      <input
+        type="file"
+        className="file:cursor-pointer file:rounded-xl file:border-0 file:bg-purple-400 file:px-5 file:py-1  file:text-white  file:hover:border file:hover:border-purple-400 file:hover:bg-white file:hover:text-purple-500"
+      />
     </div>
   );
 };
