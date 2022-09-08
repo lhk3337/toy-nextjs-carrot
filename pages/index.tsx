612fb7bf6da47609984b6a2d8ac9b94c8240b844
lhk3337 <lhk3337@naver.com>
2022-09-08 23:20:35 +0900

    [FEAT] main page style with tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,21 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className="flex flex-col space-y-5 py-10">
       {Array(12)
         .fill("")
         .map((_, i) => (
-          <div key={i}>
-            <div>
-              <div />
-              <div>
-                <h3>New iPhone 14</h3>
-                <span>Black</span>
-                <span>$95</span>
+          <div key={i} className="flex cursor-pointer justify-between border-b px-4 pb-4">
+            <div className="flex items-center space-x-5">
+              <div className="h-20 w-20 rounded-md bg-gray-400" />
+              <div className="flex flex-col">
+                <h3 className="text-sm font-medium text-gray-900">New iPhone 14</h3>
+                <span className="text-xs text-gray-500">Black</span>
+                <span className="text-gray-9000 mt-1 font-bold">$95</span>
               </div>
             </div>
-            <div>
-              <div>
+            <div className="flex items-end justify-end space-x-3">
+              <div className="flex items-center space-x-0.5 text-sm text-gray-600 ">
                 <svg
                   className="h-4 w-4"
                   fill="none"
@@ -33,7 +33,7 @@ const Home: NextPage = () => {
                 </svg>
                 <span>1</span>
               </div>
-              <div>
+              <div className="flex items-center space-x-0.5 text-sm text-gray-600 ">
                 <svg
                   className="h-4 w-4"
                   fill="none"
@@ -53,7 +53,7 @@ const Home: NextPage = () => {
             </div>
           </div>
         ))}
-      <button>
+      <button className="fixed bottom-14 right-5 cursor-pointer rounded-full bg-orange-400 p-4 text-white shadow-xl transition-colors hover:bg-orange-500">
         <svg
           className="h-6 w-6"
           xmlns="http://www.w3.org/2000/svg"
