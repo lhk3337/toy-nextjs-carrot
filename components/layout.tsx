887e61cc0f9a7eeb8115f405807e27037a3f1287
lhk3337 <lhk3337@naver.com>
2022-09-13 23:44:34 +0900

    [FEAT] Layout Component 추가

---
+++ components/layout.tsx
@@ -0,0 +1,143 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { ReactNode } from "react";
+import { cls } from "../libs/utils";
+
+interface LayoutProps {
+  title?: string;
+  canGoBack?: boolean;
+  children: ReactNode;
+  hasTabBar?: boolean;
+}
+
+export default function Layout({ title, canGoBack, hasTabBar, children }: LayoutProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.back();
+  };
+  return (
+    <div>
+      <div
+        className={cls(
+          !canGoBack ? "justify-center" : "",
+          "fixed top-0 flex w-full max-w-lg items-center space-x-2 border-b bg-white py-3 px-3 text-lg font-medium text-gray-700"
+        )}
+      >
+        {canGoBack ? (
+          <button onClick={onClick}>
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+            </svg>
+          </button>
+        ) : null}
+        {title ? <span>{title}</span> : null}
+      </div>
+      <div className={cls("pt-12", hasTabBar ? "pb-24" : "")}>{children}</div>
+      {hasTabBar ? (
+        <nav className="fixed bottom-0 flex w-full max-w-lg items-center justify-between border-t bg-white px-10 pb-5 pt-3 text-xs text-gray-800">
+          <Link href="/">
+            <a className="flex flex-col items-center space-y-2">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+              </svg>
+              <span>Home</span>
+            </a>
+          </Link>
+          <Link href="/community">
+            <a className="flex flex-col items-center space-y-2">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
+                />
+              </svg>
+              <span>D N Life</span>
+            </a>
+          </Link>
+          <Link href="/chats">
+            <a className="flex flex-col items-center space-y-2">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                />
+              </svg>
+              <span>Chat</span>
+            </a>
+          </Link>
+          <Link href="/streams">
+            <a className="flex flex-col items-center space-y-2">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+                />
+              </svg>
+              <span>Live</span>
+            </a>
+          </Link>
+          <Link href="/profile">
+            <a className="flex flex-col items-center space-y-2">
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                />
+              </svg>
+              <span>User</span>
+            </a>
+          </Link>
+        </nav>
+      ) : null}
+    </div>
+  );
+}
