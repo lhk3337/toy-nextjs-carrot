6ba27ff49fd2706ceac01275a8ec3c67a81f9270
lhk3337 <lhk3337@naver.com>
2022-09-14 11:27:48 +0900

    [FIX] back btn 화살표 변경

--- components/layout.tsx
+++ components/layout.tsx
@@ -32,7 +32,7 @@ export default function Layout({ title, canGoBack, hasTabBar, children }: Layout
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
             </svg>
           </button>
         ) : null}
