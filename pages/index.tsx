830549ea985040b32d500a02a77b85bf6164a2ca
lhk3337 <lhk3337@naver.com>
2022-09-16 17:51:16 +0900

    [FEAT] react-hook-form register 적용하기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,15 @@
 import type { NextPage } from "next";
-import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Home: NextPage = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [formErrors, setFormErrors] = useState("");
-  const [emailErrors, setEmailErrors] = useState("");
-  const onUsernameChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setUsername(value);
-    setFormErrors("");
-  };
-
-  const onEmailChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setEmail(value);
-    setEmailErrors("");
-    setFormErrors("");
-  };
-
-  const onPasswordChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setPassword(value);
-    setFormErrors("");
-  };
-  const onSubmit = (event: React.SyntheticEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (username === "" || email === "" || password === "") {
-      setFormErrors("All fields are required");
-    }
-    if (!email.includes("@")) {
-      setEmailErrors("email is required");
-    }
-  };
+  const { register } = useForm();
 
   return (
-    <form onSubmit={onSubmit}>
-      <input value={username} type="text" placeholder="Username" onChange={onUsernameChange} required />
-      <input value={email} type="email" placeholder="Email" onChange={onEmailChange} required />
-      <input value={password} type="password" placeholder="Password" onChange={onPasswordChange} required />
+    <form>
+      <input {...register("username")} type="text" placeholder="Username" required />
+      <input {...register("email")} type="email" placeholder="Email" required />
+      <input {...register("password")} type="password" placeholder="Password" required />
       <input type="submit" value="Create Account" />
-      {emailErrors}
-      {formErrors}
     </form>
   );
 };
