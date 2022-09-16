0d9612b22600accc468cc360b578c469c554a8ec
lhk3337 <lhk3337@naver.com>
2022-09-16 19:59:32 +0900

    [FEAT] form 유효성 검사

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,15 @@ import type { NextPage } from "next";
 import { useForm } from "react-hook-form";
 
 const Home: NextPage = () => {
-  const { register } = useForm();
-
+  const { register, handleSubmit } = useForm();
+  const onValid = () => {
+    console.log("valid");
+  };
   return (
-    <form>
-      <input {...register("username")} type="text" placeholder="Username" required />
-      <input {...register("email")} type="email" placeholder="Email" required />
-      <input {...register("password")} type="password" placeholder="Password" required />
+    <form onSubmit={handleSubmit(onValid)}>
+      <input {...register("username", { required: "error" })} type="text" placeholder="Username" />
+      <input {...register("email", { required: true })} type="email" placeholder="Email" />
+      <input {...register("password", { required: true })} type="password" placeholder="Password" />
       <input type="submit" value="Create Account" />
     </form>
   );
