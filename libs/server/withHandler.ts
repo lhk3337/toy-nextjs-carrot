787ac1a705f91f7f176cdc34424812e17dd49ba4
lhk3337 <lhk3337@naver.com>
2022-09-20 18:09:42 +0900

    [FEAT] withHandler with HOF

---
+++ libs/server/withHandler.ts
@@ -0,0 +1,32 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function withHandler(
+  methods: "GET" | "POST" | "DELETE",
+  handler: (req: NextApiRequest, res: NextApiResponse) => void
+) {
+  return async function (req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== methods) {
+      return res.status(405).end();
+    }
+    try {
+      handler(req, res);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ error });
+    }
+  };
+}
+
+/*
+HOF(Higher Order Function)
+const withHand = function (value: number, handlers: (value: number) => number) {
+  return handlers(handlers(value));
+};
+ // ============================================================
+
+const handlers = function (value: number) {
+  return value + 3;
+};
+
+console.log(withHand(7, handlers));
+*/
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,11 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/server/client";
+import withHandler from "../../../libs/server/withHandler";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== "POST") {
-    res.status(401).end();
-  }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
-  res.json({ ok: true });
   res.status(200).end();
 }
+
+export default withHandler("POST", handler);
