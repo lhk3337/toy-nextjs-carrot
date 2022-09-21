3c0ee69921c4e68d66edd396b916fd7f1f43d696
lhk3337 <lhk3337@naver.com>
2022-09-21 19:26:45 +0900

    [FEAT] enter api logic with prisma

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,7 +4,43 @@ import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  let user;
+  const {
+    body: { email, phone },
+  } = req;
+
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) console.log("Found it.");
+    if (!user) {
+      console.log("Did not Found, will Create!");
+      user = await client.user.create({
+        data: { name: "Anonymous", email },
+      });
+    }
+    console.log(user);
+  }
+
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    });
+    if (user) console.log("Found it.");
+    if (!user) {
+      console.log("Did not Found, will Create!");
+      user = await client.user.create({
+        data: { name: "Anonymous", phone: +phone },
+      });
+    }
+    console.log(user);
+  }
+
   res.status(200).end();
 }
 
