a5f46d5a8ee667356825c89757779fad8fa72db2
lhk3337 <lhk3337@naver.com>
2022-09-24 18:46:35 +0900

    [FEAT] iron-session 적용하기

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -2,14 +2,36 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withIronSessionApiRoute } from "iron-session/next";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: { id: number };
+  }
+}
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   const {
     body: { token },
   } = req;
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) return res.status(404);
 
-  console.log(token);
+  req.session.user = {
+    id: exists.userId,
+  };
+  await req.session.save();
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "989rruHJsJFFNe5c06Qgh2WxGYmMeQMP",
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
+});
