eef2487952b7b4a994c046f0960406d027fa622b
lhk3337 <lhk3337@naver.com>
2022-09-27 20:11:37 +0900

    [FEAT] main page - product api 데이터 가져오기 및 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,23 @@ import FixedButton from "@components/fixedCircleBtn";
 import Layout from "@components/layout";
 import Items from "@components/items";
 import useUser from "@libs/client/useUser";
+import useSWR from "swr";
+import { Product } from "@prisma/client";
+
+interface ProductResponse {
+  ok: boolean;
+  products: Product[];
+}
+
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
-  console.log(user);
+  const { data, error } = useSWR<ProductResponse>("api/products");
   return (
     <Layout title="Home" hasTabBar>
       <div className="flex flex-col space-y-5 divide-y">
-        {Array(12)
-          .fill("")
-          .map((_, i) => (
-            <Items key={i} id={i} title="New iPhone 14" option="Black" price={95} heart={1} comment={1} />
-          ))}
+        {data?.products?.map((product) => (
+          <Items key={product.id} id={product.id} title={product.name} price={product.price} heart={1} comment={1} />
+        ))}
         <FixedButton type="add" href="/products/upload" />
       </div>
     </Layout>
