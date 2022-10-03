332543cd81b953ac9a7b30c724af865811bd58c6
lhk3337 <lhk3337@naver.com>
2022-10-04 00:20:15 +0900

    [FEAT] answer api 설정하기

---
+++ pages/api/posts/[id]/answers.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const {
+    query: { id },
+    session: { user },
+    body: { answer },
+  } = req;
+  const newAnswer = await client.answer.create({
+    data: {
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+      post: {
+        connect: {
+          id: Number(id),
+        },
+      },
+      answer,
+    },
+  });
+  res.json({ ok: true, answer: newAnswer });
+}
+
+export default withApiSession(withHandler({ methods: ["POST"], handler }));
