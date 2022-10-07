977c1b6c6a0744c6d2ca431dc3ba14cf94229263
lhk3337 <lhk3337@naver.com>
2022-10-07 12:50:10 +0900

    [FEAT] profile - reviews api Handler

---
+++ pages/api/reviews.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const {
+    session: { user },
+  } = req;
+  const reviews = await client.review.findMany({
+    where: {
+      createdForId: user?.id,
+    },
+    include: {
+      createdBy: {
+        select: {
+          name: true,
+          id: true,
+          avartar: true,
+        },
+      },
+    },
+  });
+
+  res.json({ ok: true, reviews });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
