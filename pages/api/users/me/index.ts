4b1496f381ab7e6acfb566592c2acadd0d91cc8d
lhk3337 <lhk3337@naver.com>
2022-10-09 00:35:15 +0900

    [FEAT] profile page - profile 수정하기 설정

--- pages/api/users/me/index.ts
+++ pages/api/users/me/index.ts
@@ -2,10 +2,84 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { withApiSession } from "@libs/server/withSession";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { json } from "stream/consumers";
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
-  const profile = await client.user.findUnique({ where: { id: req.session.user?.id } });
-  res.json({ ok: true, profile });
+  if (req.method === "GET") {
+    const profile = await client.user.findUnique({ where: { id: req.session.user?.id } });
+    res.json({ ok: true, profile });
+  }
+
+  if (req.method === "POST") {
+    const {
+      session: { user },
+      body: { email, phone, name },
+    } = req;
+    const currentUser = await client.user.findUnique({
+      where: {
+        id: user?.id,
+      },
+    });
+    // 백엔드에서 currentUser의 email과 Edit page에서 onSubmit한 email의 데이터가 같으면 검증과 이메일 업데이트를 하지 않는다.
+    if (email && email !== currentUser?.email) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            email,
+          },
+          select: {
+            id: true,
+          },
+        })
+      );
+      // alreadyExists변수는 백엔드의 데이터와 입력 데이터의 값이 같은면 true, 다르면 false를 출력한다.
+      if (alreadyExists) {
+        return res.json({ ok: false, error: "Email already exists. Please use another email." });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: { email },
+      });
+      res.json({ ok: true });
+    }
+
+    if (phone && phone !== currentUser?.phone) {
+      const alreadyExists = Boolean(
+        await client.user.findUnique({
+          where: {
+            phone,
+          },
+          select: {
+            id: true,
+          },
+        })
+      );
+      if (alreadyExists) {
+        return res.json({ ok: false, error: "Phone number already exists. Please use another Phone number." });
+      }
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: { phone },
+      });
+      res.json({ ok: true });
+    }
+
+    if (name) {
+      await client.user.update({
+        where: {
+          id: user?.id,
+        },
+        data: {
+          name,
+        },
+      });
+    }
+    res.json({ ok: true });
+  }
 }
 
-export default withApiSession(withHandler({ methods: ["GET"], handler }));
+export default withApiSession(withHandler({ methods: ["GET", "POST"], handler }));
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -2,10 +2,70 @@ import type { NextPage } from "next";
 import Layout from "@components/layout";
 import Input from "@components/input";
 import Button from "@components/button";
+import useUser from "@libs/client/useUser";
+import { useForm } from "react-hook-form";
+import { useEffect } from "react";
+import useMutation from "@libs/client/useMutation";
+import { useRouter } from "next/router";
+
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  name?: string;
+  formErrors?: string;
+}
+
+interface EditProfileResponse {
+  ok: boolean;
+  error?: string;
+}
+
 const Edit: NextPage = () => {
+  const { user } = useUser();
+
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+
+  useEffect(() => {
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+    if (user?.name) setValue("name", user.name);
+    // 백엔드에서 데이터 불러와 input에 출력하기
+  }, [user, setValue]);
+
+  const [editProfile, { data, loading }] = useMutation<EditProfileResponse>("/api/users/me");
+
+  const onValid = ({ email, phone, name }: EditProfileForm) => {
+    if (loading) return;
+    if (email === "" && phone === "" && name === "") {
+      return setError("formErrors", {
+        message: "Email or phone number are required. Please enter one of the two.",
+      });
+    }
+    editProfile({ email, phone, name });
+  };
+
+  useEffect(() => {
+    if (data && !data.ok && data.error) {
+      setError("formErrors", { message: data.error });
+    }
+  }, [data, setError]);
+
+  const router = useRouter();
+  useEffect(() => {
+    if (data?.ok) {
+      router.push(`/profile`);
+    }
+  }, [data, router]);
+
   return (
     <Layout canGoBack title="Edit Profile">
-      <form className="space-y-4 py-10 px-4">
+      <form className="space-y-4 py-10 px-4" onSubmit={handleSubmit(onValid)}>
         <div className="flex items-center space-x-4">
           <div className="h-14 w-14 rounded-full bg-slate-500" />
           <label
@@ -17,12 +77,40 @@ const Edit: NextPage = () => {
           </label>
         </div>
         <div className="space-y-1">
-          <Input label="Email address" name="email" type="email" placeholder="이메일을 입력해주세요" required />
+          <Input
+            register={register("name")}
+            label="Name"
+            name="email"
+            type="text"
+            placeholder="이름을 입력해주세요"
+            required={false}
+          />
         </div>
         <div className="space-y-1">
-          <Input kind="phone" label="Phone number" name="phone" type="number" required />
+          <Input
+            register={register("email")}
+            label="Email address"
+            name="email"
+            type="email"
+            placeholder="이메일을 입력해주세요"
+            required={false}
+          />
         </div>
-        <Button text="Update Profile" />
+        <div className="space-y-1">
+          <Input
+            register={register("phone")}
+            kind="phone"
+            label="Phone number"
+            name="phone"
+            type="number"
+            placeholder="전화번호를 입력해주세요"
+            required={false}
+          />
+        </div>
+        {errors.formErrors ? (
+          <span className="my-1 block text-center text-sm font-bold text-red-500">{errors.formErrors?.message}</span>
+        ) : null}
+        <Button text={loading ? "Loading..." : "Update Profile"} />
       </form>
     </Layout>
   );
