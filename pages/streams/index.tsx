4713ad32738a972dc2e37a291ee95bb49e3909b9
lhk3337 <lhk3337@naver.com>
2022-10-10 23:30:58 +0900

    [FEAT] Stream page - stream list data 받아와 출력하기

--- pages/streams/index.tsx
+++ pages/streams/index.tsx
@@ -2,23 +2,30 @@ import type { NextPage } from "next";
 import Layout from "@components/layout";
 import FixedButton from "@components/fixedCircleBtn";
 import Link from "next/link";
-const Live: NextPage = () => {
+import { Stream } from "@prisma/client";
+import useSWR from "swr";
+
+interface StreamsResponse {
+  ok: boolean;
+  streams: Stream[];
+}
+
+const Streams: NextPage = () => {
+  const { data } = useSWR<StreamsResponse>("/api/streams");
   return (
     <Layout title="Live" hasTabBar>
       <div className="space-y-4 divide-y-[1px]">
-        {Array(5)
-          .fill("")
-          .map((_, i) => (
-            <Link key={i} href={`/streams/${i}`}>
-              <a className="px-4 pt-4">
-                <div className="aspect-video w-full rounded-md bg-slate-300" />
-                <h1 className="mt-2 text-2xl font-bold text-gray-700">Galaxy S50</h1>
-              </a>
-            </Link>
-          ))}
+        {data?.streams.map((stream) => (
+          <Link key={stream.id} href={`/streams/${stream.id}`}>
+            <a className="px-4 pt-4">
+              <div className="aspect-video w-full rounded-md bg-slate-300" />
+              <h1 className="mt-2 text-2xl font-bold text-gray-700">{stream.name}</h1>
+            </a>
+          </Link>
+        ))}
         <FixedButton type="video" href="/streams/create" />
       </div>
     </Layout>
   );
 };
-export default Live;
+export default Streams;
