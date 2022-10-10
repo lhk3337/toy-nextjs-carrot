6bf80fcdf8a3d99453371ee72d382d0db2ef6f33
lhk3337 <lhk3337@naver.com>
2022-10-10 19:47:55 +0900

    [REFACT] live stream Create page Input Component 추가하기

--- pages/lives/create.tsx
+++ pages/lives/create.tsx
@@ -2,46 +2,23 @@ import type { NextPage } from "next";
 import Button from "@components/button";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
+import Input from "@components/input";
+
 const Create: NextPage = () => {
   return (
     <Layout canGoBack title="Create Live Stream">
-      <div className="space-y-5 py-10 px-4">
+      <form className="space-y-5 py-10 px-4">
         <div>
-          <label className="mb-1 block text-sm font-medium text-gray-500" htmlFor="name">
-            Name
-          </label>
-          <div className="relative flex items-center rounded-md shadow-sm">
-            <input
-              type="text"
-              id="name"
-              className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
-            />
-          </div>
+          <Input name="name" label="Name" type="text" required />
         </div>
         <div>
-          <label className="mb-1 block text-sm font-medium text-gray-500" htmlFor="price">
-            Price
-          </label>
-          <div className="relative flex items-center rounded-md shadow-sm">
-            <div className="items-cetner pointer-events-none absolute left-0 flex justify-center pl-3">
-              <span className="text-sm text-gray-500">$</span>
-            </div>
-            <input
-              type="text"
-              id="price"
-              placeholder="0.00"
-              className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 pl-7 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
-            />
-            <div className="pointer-events-none  absolute right-0 flex items-center pr-3">
-              <span className="text-gray-500">USD</span>
-            </div>
-          </div>
+          <Input name="price" label="Price" type="text" kind="price" placeholder="0.00" required />
         </div>
         <div>
-          <TextArea label="Description" name="desc" rows={4} />
+          <TextArea label="Description" name="desc" rows={4} required />
         </div>
         <Button text="Go Live" />
-      </div>
+      </form>
     </Layout>
   );
 };
