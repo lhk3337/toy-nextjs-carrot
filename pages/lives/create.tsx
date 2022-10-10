198376f0d29af31408ecb9e477e4d3eccd85e90e
lhk3337 <lhk3337@naver.com>
2022-10-10 19:52:12 +0900

    [FEAT] live stream Create page react-hook-form 추가

--- pages/lives/create.tsx
+++ pages/lives/create.tsx
@@ -3,19 +3,44 @@ import Button from "@components/button";
 import Layout from "@components/layout";
 import TextArea from "@components/textarea";
 import Input from "@components/input";
+import { useForm } from "react-hook-form";
+interface CreateForm {
+  name: string;
+  price: number;
+  description: string;
+}
 
 const Create: NextPage = () => {
+  const { register, handleSubmit } = useForm<CreateForm>();
+  const onValid = (data: CreateForm) => {
+    console.log(data);
+  };
+
   return (
     <Layout canGoBack title="Create Live Stream">
-      <form className="space-y-5 py-10 px-4">
+      <form className="space-y-5 py-10 px-4" onSubmit={handleSubmit(onValid)}>
         <div>
-          <Input name="name" label="Name" type="text" required />
+          <Input register={register("name", { required: true })} name="name" label="Name" type="text" required />
         </div>
         <div>
-          <Input name="price" label="Price" type="text" kind="price" placeholder="0.00" required />
+          <Input
+            register={register("price", { required: true })}
+            name="price"
+            label="Price"
+            type="text"
+            kind="price"
+            placeholder="0.00"
+            required
+          />
         </div>
         <div>
-          <TextArea label="Description" name="desc" rows={4} required />
+          <TextArea
+            label="Description"
+            name="desc"
+            rows={4}
+            register={register("description", { required: true })}
+            required
+          />
         </div>
         <Button text="Go Live" />
       </form>
