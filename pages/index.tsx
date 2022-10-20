a7aa430fd75979399c9522727e4fb5292bc3637b
lhk3337 <lhk3337@naver.com>
2022-10-20 15:43:47 +0900

    [FEAT] nextjs Image remote Image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import Image from "next/image";
-import riceCake from "../public/local.jpeg";
 
 const Home: NextPage = () => {
+  const imageUrl =
+    "https://images.unsplash.com/photo-1532117364815-720cd35ff6e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3000&q=80";
   return (
-    <div>
-      <Image src={riceCake} placeholder="blur" />
+    <div className="mt-96 flex items-center justify-center">
+      <Image src={imageUrl} className=" h-16 w-16 rounded-full" width={64} height={64} />
     </div>
   );
 };
