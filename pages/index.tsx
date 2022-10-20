dc71f24e41f3b4e983b8cd2ad052bfc8cc816dc0
lhk3337 <lhk3337@naver.com>
2022-10-20 17:39:16 +0900

    [FEAT] nextjs Image layout fill

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,14 @@ const Home: NextPage = () => {
   const imageUrl =
     "https://images.unsplash.com/photo-1532117364815-720cd35ff6e3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3000&q=80";
   return (
-    <div className="mt-96 flex items-center justify-center">
-      <Image src={imageUrl} className=" h-16 w-16 rounded-full" width={64} height={64} />
-    </div>
+    <>
+      <div className="relative pb-56">
+        <Image src={imageUrl} className="bg-slate-300 object-contain" layout="fill" alt="layoutfill" />
+      </div>
+      <div className="mt-96 flex h-60 w-60 items-center justify-between">
+        <Image src={imageUrl} className=" h-16 w-16 rounded-full" width={64} height={64} alt="notlayout" />
+      </div>
+    </>
   );
 };
 export default Home;
