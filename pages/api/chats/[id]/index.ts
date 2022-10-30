25a736e28d483c5aad48a491f2c7804a08faf104
lhk3337 <lhk3337@naver.com>
2022-10-30 17:12:46 +0900

    [FEAT] Chat detail and chat message api handler

---
+++ pages/api/chats/[id]/index.ts
@@ -0,0 +1,43 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const {
+    query: { id },
+  } = req;
+  const chat = await client.chat.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      messages: { select: { message: true, id: true, user: { select: { avatar: true, id: true } } } },
+      product: {
+        select: {
+          price: true,
+          imageUrl: true,
+          name: true,
+          id: true,
+        },
+      },
+      seller: {
+        select: {
+          id: true,
+          avatar: true,
+          name: true,
+        },
+      },
+      buyer: {
+        select: {
+          id: true,
+          avatar: true,
+          name: true,
+        },
+      },
+    },
+  });
+  return res.status(200).json({ ok: true, chat });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
---
+++ pages/api/chats/[id]/message.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
+  const {
+    query: { id },
+    body,
+    session: { user },
+  } = req;
+  const message = await client.message.create({
+    data: {
+      message: body.message,
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+      chat: {
+        connect: {
+          id: Number(id),
+        },
+      },
+    },
+  });
+  res.json({ ok: true, message });
+}
+
+export default withApiSession(withHandler({ methods: ["POST"], handler }));
