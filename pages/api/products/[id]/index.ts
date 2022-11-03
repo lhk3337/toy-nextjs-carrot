5d330727c39007c5d4ba7d18ad99c142dd9ddab3
lhk3337 <lhk3337@naver.com>
2022-11-03 20:00:55 +0900

    [FEAT] 상품 판매 상태 변경 API Handler

--- pages/api/products/[id]/index.ts
+++ pages/api/products/[id]/index.ts
@@ -6,44 +6,60 @@ import { withApiSession } from "@libs/server/withSession";
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
   const {
     query: { id },
+    body: { sellState },
     session: { user },
   } = req;
-  const product = await client.product.findUnique({
-    where: { id: Number(id) },
-    include: { user: { select: { id: true, name: true, avatar: true } } },
-  });
-  const chat = await client.chat.findFirst({
-    where: { buyerId: user?.id, productId: Number(id) },
-  });
-  const chatId = chat?.id;
-  const terms = product?.name.split(" ").map((word) => ({
-    name: {
-      contains: word,
-    },
-  }));
-  const relatedProducts = await client.product.findMany({
-    where: {
-      OR: terms,
-      AND: {
-        id: {
-          not: product?.id,
+  if (req.method === "GET") {
+    const product = await client.product.findUnique({
+      where: { id: Number(id) },
+      include: { user: { select: { id: true, name: true, avatar: true } } },
+    });
+
+    const terms = product?.name.split(" ").map((word) => ({
+      name: {
+        contains: word,
+      },
+    }));
+    const relatedProducts = await client.product.findMany({
+      where: {
+        OR: terms,
+        AND: {
+          id: {
+            not: product?.id,
+          },
         },
       },
-    },
-  });
-  const isLiked = Boolean(
-    await client.record.findFirst({
-      where: {
-        productId: Number(id),
-        userId: user?.id,
-        kind: "Fav",
-      },
-      select: {
-        id: true,
-      },
-    })
-  );
-  res.json({ ok: true, product, chatId, isLiked, relatedProducts });
-}
+    });
 
-export default withApiSession(withHandler({ methods: ["GET"], handler }));
+    const isLiked = Boolean(
+      await client.record.findFirst({
+        where: {
+          productId: Number(id),
+          userId: user?.id,
+          kind: "Fav",
+        },
+        select: {
+          id: true,
+        },
+      })
+    );
+
+    const chat = await client.chat.findFirst({
+      where: { buyerId: user?.id, productId: Number(id) },
+    });
+    const chatId = chat?.id;
+
+    res.json({ ok: true, product, chatId, isLiked, relatedProducts });
+  }
+  if (req.method === "POST") {
+    await client.product.update({
+      where: { id: Number(id) },
+      data: {
+        sellState,
+      },
+    });
+
+    res.json({ ok: true });
+  }
+}
+export default withApiSession(withHandler({ methods: ["GET", "POST"], handler }));
