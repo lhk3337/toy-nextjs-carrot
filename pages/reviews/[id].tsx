db708e3cf990ac0b762a3a5aafa189d3dc75f539
lhk3337 <lhk3337@naver.com>
2022-11-16 18:02:03 +0900

    [FEAT] skeleton loading, 다른아이디가 해당 url 접속시 handler

--- pages/reviews/[id].tsx
+++ pages/reviews/[id].tsx
@@ -3,6 +3,7 @@ import Layout from "@components/layout";
 import StarRating from "@components/starRating";
 import TextArea from "@components/textarea";
 import useMutation from "@libs/client/useMutation";
+import useUser from "@libs/client/useUser";
 import { Review } from "@prisma/client";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
@@ -23,6 +24,8 @@ const Review: NextPage = () => {
   const { register, handleSubmit, reset, setValue } = useForm<ReviewForm>();
 
   const router = useRouter();
+  const { user } = useUser();
+
   const { data } = useSWR<ReviewData>(router.query.id ? `/api/reviews/${router.query.id}` : null);
 
   const [starRating, setStarRating] = useState<number>(0);
@@ -30,10 +33,12 @@ const Review: NextPage = () => {
   const [reviews] = useMutation(router.query.id ? `/api/reviews/${router.query.id}` : "");
 
   useEffect(() => {
-    if (data?.reviewData?.review) {
-      setValue("review", data?.reviewData.review);
+    if (data?.reviewData.createdById === user?.id) {
+      if (data?.reviewData?.review) {
+        setValue("review", data?.reviewData.review);
+      }
     }
-  }, [setValue, data]);
+  }, [setValue, data, user]);
 
   useEffect(() => {
     if (data?.reviewData?.score) {
@@ -41,26 +46,46 @@ const Review: NextPage = () => {
     }
   }, [setStarRating, data]);
 
-  const onValid = (data: ReviewForm) => {
-    reviews({ ...data, starRating });
-    reset();
-    router.back();
+  const onValid = (dataForm: ReviewForm) => {
+    if (data?.reviewData.createdById === user?.id) {
+      reviews({ ...dataForm, starRating });
+      reset();
+      router.back();
+    }
   };
-
+  const onBackClick = () => {
+    router.replace("/chats");
+  };
   return (
-    <Layout canGoBack title="리뷰 쓰기">
-      <form onSubmit={handleSubmit(onValid)} className="space-y-4 px-4 py-6">
-        {/* star Rating Components */}
-        <StarRating setStarRating={setStarRating} starRating={starRating} />
-        <TextArea
-          register={register("review", { required: true, minLength: 5 })}
-          rows={8}
-          placeholder="Write a review"
-          required
-        />
-        <Button text="Submit" />
-      </form>
-    </Layout>
+    <>
+      {!data ? (
+        <div className="flex flex-col space-y-3 py-4">
+          <div className=" rounded-lg border-[1px] bg-slate-200  px-32 py-6" />
+          <div className=" rounded-lg border-[1px] bg-slate-200  px-32 py-12 " />
+          <div className=" rounded-lg border-[1px] bg-slate-200  px-32 py-36" />
+        </div>
+      ) : (
+        <Layout canGoBack title="리뷰 쓰기">
+          {data?.reviewData.createdById !== user?.id ? (
+            <div className="py-10">
+              <Button large text="되돌아가기" onClick={onBackClick} />
+            </div>
+          ) : (
+            <form onSubmit={handleSubmit(onValid)} className="space-y-4 px-4 py-6">
+              {/* star Rating Components */}
+              <StarRating setStarRating={setStarRating} starRating={starRating} />
+              <TextArea
+                register={register("review", { required: true, minLength: 5 })}
+                rows={8}
+                placeholder="Write a review"
+                required
+              />
+              <Button text="Submit" />
+            </form>
+          )}
+        </Layout>
+      )}
+    </>
   );
 };
 export default Review;
