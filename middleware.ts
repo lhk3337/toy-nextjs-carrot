fb4cedac79c3ccaa89d352baa3c2905d3d809e8a
lhk3337 <lhk3337@naver.com>
2022-11-18 21:48:19 +0900

    [FEAT]  login할때 cookie  입증 확인하기 - middleware

---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextFetchEvent, NextRequest, NextResponse, userAgent } from "next/server";
+
+export const middleware = (req: NextRequest, ev: NextFetchEvent) => {
+  if (userAgent(req).isBot) {
+    // return new Response("Plz don't be a bot. Be human",{status:403});
+  }
+
+  if (!req.url.includes("/api")) {
+    if (!req.cookies.has("carrotsession") && !req.url.includes("/enter")) {
+      req.nextUrl.searchParams.set("from", req.nextUrl.pathname);
+      req.nextUrl.pathname = "/enter";
+      return NextResponse.redirect(req.nextUrl);
+    }
+  }
+};
+
+export const config = {
+  matcher: ["/((?!api|_next/static|favicon.ico).*)"],
+};
