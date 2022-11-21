f7f623722a36f3ed05afb8395a78a58cc6a4f800
lhk3337 <lhk3337@naver.com>
2022-11-21 18:29:27 +0900

    [nextjs Deep Dive] Custom Document

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+class CustomDocument extends Document {
+  render(): JSX.Element {
+    console.log("DOCUMENT IS RUNNING");
+    return (
+      <Html lang="ko">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@900&family=Open+Sans&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
