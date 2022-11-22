5f2cfb3ca4abe70107d094916b1786fcd55f2a82
lhk3337 <lhk3337@naver.com>
2022-11-22 22:01:08 +0900

    [FEAT] bottom bar 아이콘 해당 페이지 클릭 시 색 변경

--- components/layout.tsx
+++ components/layout.tsx
@@ -14,6 +14,7 @@ interface LayoutProps {
 
 export default function Layout({ title, canGoBack, hasBottomTabBar, children, seoTitle }: LayoutProps) {
   const router = useRouter();
+
   const onClick = () => {
     router.back();
   };
@@ -49,7 +50,7 @@ export default function Layout({ title, canGoBack, hasBottomTabBar, children, se
           <Link href="/">
             <a className="flex flex-col items-center space-y-2">
               <svg
-                className="h-6 w-6"
+                className={cls("h-6 w-6 font-bold", router.pathname === "/" ? "text-orange-500" : "")}
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -62,13 +63,13 @@ export default function Layout({ title, canGoBack, hasBottomTabBar, children, se
                   d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                 />
               </svg>
-              <span>Home</span>
+              <span className={cls(router.pathname === "/" ? " text-orange-500" : "")}>홈</span>
             </a>
           </Link>
           <Link href="/community">
             <a className="flex flex-col items-center space-y-2">
               <svg
-                className="h-6 w-6"
+                className={cls("h-6 w-6 font-bold", router.pathname === "/community" ? "text-orange-500" : "")}
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -81,13 +82,13 @@ export default function Layout({ title, canGoBack, hasBottomTabBar, children, se
                   d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
                 />
               </svg>
-              <span>D N Life</span>
+              <span className={cls(router.pathname === "/community" ? " text-orange-500" : "")}>동네생활</span>
             </a>
           </Link>
           <Link href="/chats">
             <a className="flex flex-col items-center space-y-2">
               <svg
-                className="h-6 w-6"
+                className={cls("h-6 w-6 font-bold", router.pathname === "/chats" ? "text-orange-500" : "")}
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -100,13 +101,13 @@ export default function Layout({ title, canGoBack, hasBottomTabBar, children, se
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 />
               </svg>
-              <span>Chat</span>
+              <span className={cls(router.pathname === "/chats" ? " text-orange-500" : "")}>채팅</span>
             </a>
           </Link>
           <Link href="/streams">
             <a className="flex flex-col items-center space-y-2">
               <svg
-                className="h-6 w-6"
+                className={cls("h-6 w-6 font-bold", router.pathname === "/streams" ? "text-orange-500" : "")}
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -119,13 +120,13 @@ export default function Layout({ title, canGoBack, hasBottomTabBar, children, se
                   d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
                 />
               </svg>
-              <span>Live</span>
+              <span className={cls(router.pathname === "/streams" ? " text-orange-500" : "")}>라이브</span>
             </a>
           </Link>
           <Link href="/profile">
             <a className="flex flex-col items-center space-y-2">
               <svg
-                className="h-6 w-6"
+                className={cls("h-6 w-6", router.pathname === "/profile" ? "font-bold text-orange-500" : "")}
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -138,7 +139,7 @@ export default function Layout({ title, canGoBack, hasBottomTabBar, children, se
                   d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                 />
               </svg>
-              <span>User</span>
+              <span className={cls(router.pathname === "/profile" ? " text-orange-500" : "")}>내 정보</span>
             </a>
           </Link>
         </nav>
