7a68a8857887f2f809b62b4db299ed03ebe5bcae
lhk3337 <lhk3337@naver.com>
2022-11-22 16:23:58 +0900

    [nextjs Deep Dive] getServerSideProps

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 class CustomDocument extends Document {
   render(): JSX.Element {
-    console.log("DOCUMENT IS RUNNING");
     return (
       <Html lang="ko">
         <Head>
