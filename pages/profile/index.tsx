c708676d0d3b4e1a8ec293acc567bbfd9c26eb45
lhk3337 <lhk3337@naver.com>
2022-11-22 19:08:47 +0900

    [nextjs Deep Dive] SSR + Authentication

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,8 +1,8 @@
-import type { NextPage } from "next";
+import type { NextPage, NextPageContext } from "next";
 import Link from "next/link";
 import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
-import useSWR from "swr";
+import useSWR, { SWRConfig } from "swr";
 import { Review, User } from "@prisma/client";
 import { cls } from "@libs/client/utils";
 import Button from "@components/button";
@@ -10,6 +10,8 @@ import useMutation from "@libs/client/useMutation";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
+import { withSsrSession } from "@libs/server/withSession";
+import client from "@libs/server/client";
 
 interface ReviewWithUser extends Review {
   createdBy: User;
@@ -187,5 +189,19 @@ const Profile: NextPage = () => {
     </Layout>
   );
 };
-
-export default Profile;
+const Page: NextPage<{ profile: User }> = ({ profile }) => (
+  <SWRConfig
+    value={{
+      fallback: { "/api/users/me": { ok: true, profile } },
+    }}
+  >
+    <Profile />
+  </SWRConfig>
+);
+export const getServerSideProps = withSsrSession(async function ({ req }: NextPageContext) {
+  const profile = await client.user.findUnique({ where: { id: req?.session.user?.id } });
+  return {
+    props: { profile: JSON.parse(JSON.stringify(profile)) },
+  };
+});
+export default Page;
