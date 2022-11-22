297591a172db269fd70ee1bc1d7ad7fa37c5840a
lhk3337 <lhk3337@naver.com>
2022-11-22 18:02:37 +0900

    [nextjs Deep Dive] getServerSideProps + useSWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import FixedButton from "@components/fixedCircleBtn";
 import Layout from "@components/layout";
 import Items from "@components/items";
 import useUser from "@libs/client/useUser";
-import useSWR from "swr";
+import useSWR, { SWRConfig } from "swr";
 import { Product } from "@prisma/client";
 
 export interface ProductWithCount extends Product {
@@ -17,13 +17,13 @@ interface ProductResponse {
   products: ProductWithCount[];
 }
 
-const Home: NextPage<{ products: ProductWithCount[] }> = ({ products }) => {
-  // const { data, error } = useSWR<ProductResponse>("api/products");
+const Home: NextPage = () => {
+  const { data, error } = useSWR<ProductResponse>("/api/products");
 
   return (
-    <Layout title="Home" hasBottomTabBar>
+    <Layout title="Home" hasBottomTabBar seoTitle="Home">
       <div className="flex flex-col space-y-5 divide-y">
-        {products?.map((product) => (
+        {data?.products?.map((product) => (
           <Items {...product} key={product.id} />
         ))}
         <FixedButton type="add" href="/products/upload" />
@@ -31,6 +31,16 @@ const Home: NextPage<{ products: ProductWithCount[] }> = ({ products }) => {
     </Layout>
   );
 };
+
+const Page: NextPage<{ products: ProductWithCount[] }> = ({ products }) => (
+  <SWRConfig
+    value={{
+      fallback: { "/api/products": { ok: true, products } },
+    }}
+  >
+    <Home />
+  </SWRConfig>
+);
 
 export async function getServerSideProps() {
   const productquerys = await client?.product.findMany({
@@ -56,4 +66,4 @@ export async function getServerSideProps() {
   };
 }
 
-export default Home;
+export default Page;
