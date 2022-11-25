7ced54d9cd5c9f7f64903041a0f346182387b231
lhk3337 <lhk3337@naver.com>
2022-11-25 16:32:11 +0900

    [FIX] Community Page

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -20,7 +20,7 @@ interface PostResponse {
   posts: PostwithUser[];
 }
 
-const Community: NextPage<PostResponse> = ({ posts }) => {
+const Community: NextPage<PostResponse> = () => {
   const { latitude, longitude } = useCoords();
   const { data } = useSWR<PostResponse>(
     latitude && longitude ? `/api/posts?latitude=${latitude}&longitude=${longitude}` : null
@@ -28,7 +28,7 @@ const Community: NextPage<PostResponse> = ({ posts }) => {
   return (
     <Layout title="DN Life" hasBottomTabBar seoTitle="DN Life | Carrot Market">
       <div className="space-y-4 divide-y-[1px]">
-        {data?.posts?.map((post) => {
+        {data?.posts.map((post) => {
           const createTime = new Date(post.createdAt);
 
           return (
@@ -60,7 +60,7 @@ const Community: NextPage<PostResponse> = ({ posts }) => {
                         d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
                       ></path>
                     </svg>
-                    <span>궁금해요 {post._count?.wonderings}</span>
+                    <span>궁금해요 {post._count.wonderings}</span>
                   </span>
                   <span className="flex items-center space-x-2 text-sm">
                     <svg
@@ -77,7 +77,7 @@ const Community: NextPage<PostResponse> = ({ posts }) => {
                         d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                       ></path>
                     </svg>
-                    <span>답변 {post._count?.answers}</span>
+                    <span>답변 {post._count.answers}</span>
                   </span>
                 </div>
               </a>
